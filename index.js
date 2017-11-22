443a18046f3f574e9a32fa318d1bd38a1adce4b6
xbonk12 <CP088217@VHA.internal>
2017-11-22 16:56:33 +1100

    - Developed a few more methods - Currently stuck on the getMatchHistory API call

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-// proxy from dev env - remove proxy options on your machine
-var rp = require('request-promise').defaults({
-    proxy: 'http://internet-proxy.dc.vodafone.com.au:8080'
-});
-
+"use strict"
 var dotaWebAPI = require('./lib/api');
-
-// "use strict"
 
 
 var test = new dotaWebAPI("1F72FF9BF1B0C1DF25A8CFA722DA0019")
-test.getMatchDetails("3574415631")
+
+// test.getMatchDetails("3574415631")
+// .then(data => console.log(data))
+
+// german 
+// test.getLeagueListing("de")
+// .then(data => console.log(data.result.leagues[0]))
+
+// english
+// test.getLeagueListing()
+// .then(data => console.log(data.result.leagues[0]))
+
+// test.getLiveLeagueGames()
+// .then(data => console.log(data.result.games[0]))
+
+// random guy with one game in his history
+test.getMatchHistory("vilst3r","1")
 .then(data => console.log(data))
+
+
 
 module.exports = require('./lib/api');
