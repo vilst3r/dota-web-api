b26461c5540f1a3ad6f8c8ce11145613be66f1bf
xbonk12 <CP088217@VHA.internal>
2017-11-22 14:05:31 +1100

    Moved appID request to the insertion point (index.js)

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// "use strict"
+// proxy from dev env - remove when cloning
+var rp = require('request-promise').defaults({
+    proxy: 'http://internet-proxy.dc.vodafone.com.au:8080'
+});
 
 var dotaWebAPI = require('./lib/api');
-var test = new dotaWebAPI("key123")
-console.log(test)
+
+// "use strict"
+
+rp("http://api.steampowered.com/ISteamApps/GetAppList/v2")
+	.then(response => JSON.parse(response))
+	.then(data => data.applist.apps.find(app => {
+		if (app.name === "Dota 2")  {
+			var test = new dotaWebAPI("key123", app.appid)
+			console.log(test.appId)
+		}
+	}))
 
 module.exports = require('./lib/api');
