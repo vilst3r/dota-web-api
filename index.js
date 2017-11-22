77878bca8839c653579056017738d7bc3f28494d
xbonk12 <CP088217@VHA.internal>
2017-11-22 16:02:24 +1100

    Removed dota 2 appId request

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// proxy from dev env - remove when cloning
+// proxy from dev env - remove proxy options on your machine
 var rp = require('request-promise').defaults({
     proxy: 'http://internet-proxy.dc.vodafone.com.au:8080'
 });
@@ -7,14 +7,9 @@ var dotaWebAPI = require('./lib/api');
 
 // "use strict"
 
-rp("http://api.steampowered.com/ISteamApps/GetAppList/v2")
-	.then(response => JSON.parse(response))
-	.then(data => data.applist.apps.find(app => {
-		if (app.name === "Dota 2")  {
-			var test = new dotaWebAPI("1F72FF9BF1B0C1DF25A8CFA722DA0019", app.appid)
-			test.getMatchDetails("3574415631")
-			.then(data => console.log(data))
-		}
-	}))
+
+var test = new dotaWebAPI("1F72FF9BF1B0C1DF25A8CFA722DA0019")
+test.getMatchDetails("3574415631")
+.then(data => console.log(data))
 
 module.exports = require('./lib/api');
