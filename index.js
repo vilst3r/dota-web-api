21fbdf7e6274e96944cd37e3c63b2a685b30d735
xbonk12 <CP088217@VHA.internal>
2017-11-23 15:53:02 +1100

    Removed env proxy from the api.js file for the published module

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 "use strict"
-var dotaWebAPI = require('./lib/api');
-
-
-var test = new dotaWebAPI("1F72FF9BF1B0C1DF25A8CFA722DA0019")
-
-// get latest game from my account vilst3r 
-test.getMatchHistory(null,null,null,null,null,null,null,null,null)
-.then(data => console.log(data.result.matches[0]))
-
-
-
-
 
 module.exports = require('./lib/api');
-
