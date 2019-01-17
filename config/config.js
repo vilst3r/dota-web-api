82e7dfa353ab7bf8d387e380c07c09dabb4ebce3
vilst3r <clifford.phan@gmail.com>
2019-01-18 09:53:35 +1100

    Add config files and factor to hold configuration for using the web interface.

---
+++ config/config.js
@@ -0,0 +1,5 @@
+const APP_ID = 570
+
+module.exports = {
+    APP_ID: APP_ID
+}
---
+++ config/urls.js
@@ -0,0 +1,9 @@
+const API_URL = 'http://api.steampowered.com/';
+const MATCH_INTERFACE = 'IDOTA2Match_';
+const ECONOMY_INTERFACE = 'IEconDOTA2_';
+
+module.exports = {
+    API_URL: API_URL,
+    MATCH_INTERFACE: MATCH_INTERFACE,
+    ECONOMY_INTERFACE: ECONOMY_INTERFACE
+}
