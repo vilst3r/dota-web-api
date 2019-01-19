3d6fb03cf175d03a1753371eab10f0a218a48105
vilst3r <clifford.phan@gmail.com>
2019-01-19 15:24:53 +1100

    Refactor parts of test + install native fetch

--- src/api.js
+++ src/api.js
@@ -1,128 +1,152 @@
+import "isomorphic-fetch";
+
 import { APP_ID } from '../config/config';
 import { API_URL, MATCH_INTERFACE, ECONOMY_INTERFACE } from '../config/urls';
 
 // API Constructor
-let dotaWebAPI = function (apiKey) {
-	this.apiKey = apiKey;
-}
+// let dotaWebAPI = function (apiKey) {
+// 	this.apiKey = apiKey;
+// }
 
-// Match Interface Methods
-dotaWebAPI.prototype.getMatchDetails = (matchId) => {
-	let parameters = {
-		"key" : this.apiKey,
-		"match_id" : matchId
-	}	
-	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetMatchDetails/v1" + query(parameters))
-			.then(response => JSON.parse(response));
-}
+class dotaWebAPI {
 
-dotaWebAPI.prototype.getLeagueListing = (lang) => {
-	let parameters = {
-		"key" : this.apiKey,
-		"language" : lang
-	}	
-	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetLeagueListing/v1/" + query(parameters))
-			.then(response => JSON.parse(response));
-}
 
-dotaWebAPI.prototype.getLiveLeagueGames = () => {
-	let parameters = {
-		"key" : this.apiKey
+	constructor(apiKey) {
+		this.apiKey = apiKey
 	}
-	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetLiveLeagueGames/v1/" + query(parameters))
-			.then(response => JSON.parse(response));
-}
 
-dotaWebAPI.prototype.getMatchHistory = (heroId, gameMode, skill, minPlayers, accountId, leagueId, startAtMatchId, matchesRequested, tournamentGamesOnly) => {
-	let parameters = {
-		"key" : this.apiKey,
-		"hero_id" : heroId, 
-		"game_mode" : gameMode,
-		"skill" : skill,
-		"minPlayers" : minPlayers,
-		"account_id" : accountId,
-		"league_id" : leagueId,
-		"start_at_match_id" : startAtMatchId,
-		"matches_requested" : matchesRequested,
-		"tournament_games_only" : tournamentGamesOnly
+	getMatchDetails(matchId) {
+		let parameters = {
+			"key": this.apiKey,
+			"match_id": matchId
+		}
+
+		return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetMatchDetails/v1" + query(parameters))
+		.then(response => JSON.parse(response));
 	}
-	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetMatchHistory/v1/" + query(parameters))
-			.then(response => JSON.parse(response))
 }
 
-dotaWebAPI.prototype.getMatchHistoryBySequenceNumber = (startMatchSeqNum, matchesRequested) => {
-	let parameters = {
-		"key" : this.apiKey,
-		"start_at_match_seq_num" : startMatchSeqNum,
-		"matches_requested" : matchesRequested
-	}
-	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetMatchHistoryBySequenceNum/v1" + query(parameters))
-			.then(response => JSON.parse(response))
-}
+// // Match Interface Methods
+// dotaWebAPI.prototype.getMatchDetails = (matchId) => {
+// 	console.log(this);
 
-// dotaWebAPI.prototype.getScheduledLeagueGames = (dateMin, dateMax) => {
 // 	let parameters = {
 // 		"key" : this.apiKey,
-// 		"date_min" : dateMin,
-// 		"date_max" : dateMax
+// 		"match_id" : matchId
+// 	}	
+	// return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetMatchDetails/v1" + query(parameters))
+	// 		.then(response => JSON.parse(response));
+// }
+
+// dotaWebAPI.prototype.getLeagueListing = (lang) => {
+// 	let parameters = {
+// 		"key" : this.apiKey,
+// 		"language" : lang
+// 	}	
+// 	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetLeagueListing/v1/" + query(parameters))
+// 			.then(response => JSON.parse(response));
+// }
+
+// dotaWebAPI.prototype.getLiveLeagueGames = () => {
+// 	let parameters = {
+// 		"key" : this.apiKey
 // 	}
-// 	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetScheduledLeagueGames/v1/" + query(parameters))
+// 	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetLiveLeagueGames/v1/" + query(parameters))
+// 			.then(response => JSON.parse(response));
+// }
+
+// dotaWebAPI.prototype.getMatchHistory = (heroId, gameMode, skill, minPlayers, accountId, leagueId, startAtMatchId, matchesRequested, tournamentGamesOnly) => {
+// 	let parameters = {
+// 		"key" : this.apiKey,
+// 		"hero_id" : heroId, 
+// 		"game_mode" : gameMode,
+// 		"skill" : skill,
+// 		"minPlayers" : minPlayers,
+// 		"account_id" : accountId,
+// 		"league_id" : leagueId,
+// 		"start_at_match_id" : startAtMatchId,
+// 		"matches_requested" : matchesRequested,
+// 		"tournament_games_only" : tournamentGamesOnly
+// 	}
+// 	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetMatchHistory/v1/" + query(parameters))
 // 			.then(response => JSON.parse(response))
 // }
 
-dotaWebAPI.prototype.getTeamInfo = (startTeamId, teamsRequested) => {
-	let parameters = {
-		"key" : this.apiKey,
-		"start_at_team_id" : startTeamId,
-		"teams_requested" : teamsRequested
-	}
-	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetTeamInfoByTeamID/v1" + query(parameters))
-			.then(response => JSON.parse(response))
-}
+// dotaWebAPI.prototype.getMatchHistoryBySequenceNumber = (startMatchSeqNum, matchesRequested) => {
+// 	let parameters = {
+// 		"key" : this.apiKey,
+// 		"start_at_match_seq_num" : startMatchSeqNum,
+// 		"matches_requested" : matchesRequested
+// 	}
+// 	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetMatchHistoryBySequenceNum/v1" + query(parameters))
+// 			.then(response => JSON.parse(response))
+// }
 
-dotaWebAPI.prototype.getTournamentPlayerStats = (accountId, leagueId, heroId, timeFrame) => {
-	let parameters = {
-		"key" : this.apiKey,
-		"account_id" : accountId,
-		"league_id" : leagueId,
-		"hero_id" : heroId,
-		"time_frame" : timeFrame
-	}
-	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetTournamentPlayerStats/v1" + query(parameters))
-			.then(response => JSON.parse(response))
-}
+// // dotaWebAPI.prototype.getScheduledLeagueGames = (dateMin, dateMax) => {
+// // 	let parameters = {
+// // 		"key" : this.apiKey,
+// // 		"date_min" : dateMin,
+// // 		"date_max" : dateMax
+// // 	}
+// // 	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetScheduledLeagueGames/v1/" + query(parameters))
+// // 			.then(response => JSON.parse(response))
+// // }
 
-// Economy Interface Methods
-dotaWebAPI.prototype.getItems = (lang) => {
-	let parameters = {
-		"key" : this.apiKey,
-		"language" : lang
-	}
-	return fetch(API_URL + ECONOMY_INTERFACE + APP_ID + "/GetGameItems/v1" + query(parameters))
-			.then(response => JSON.parse(response))
-}
+// dotaWebAPI.prototype.getTeamInfo = (startTeamId, teamsRequested) => {
+// 	let parameters = {
+// 		"key" : this.apiKey,
+// 		"start_at_team_id" : startTeamId,
+// 		"teams_requested" : teamsRequested
+// 	}
+// 	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetTeamInfoByTeamID/v1" + query(parameters))
+// 			.then(response => JSON.parse(response))
+// }
 
-dotaWebAPI.prototype.getHeroes = (lang, ifItemized) => {
-	let parameters = {
-		"key" : this.apiKey,
-		"language" : lang,
-		"itemizedonly" : ifItemized
-	}
-	return fetch(API_URL + ECONOMY_INTERFACE + APP_ID + "/GetHeroes/v1" + query(parameters))
-			.then(response => JSON.parse(response))
-}
+// dotaWebAPI.prototype.getTournamentPlayerStats = (accountId, leagueId, heroId, timeFrame) => {
+// 	let parameters = {
+// 		"key" : this.apiKey,
+// 		"account_id" : accountId,
+// 		"league_id" : leagueId,
+// 		"hero_id" : heroId,
+// 		"time_frame" : timeFrame
+// 	}
+// 	return fetch(API_URL + MATCH_INTERFACE + APP_ID + "/GetTournamentPlayerStats/v1" + query(parameters))
+// 			.then(response => JSON.parse(response))
+// }
 
-dotaWebAPI.prototype.getItemIconPath = (name) => {
-	name = name.replace(/item_/gi, '') + '_';
-	return 'http://cdn.dota2.com/apps/dota2/images/items/' + name + 'lg.png';
-}
-// sb.png, lg.png, full.png, vert.jpg
-dotaWebAPI.prototype.getHeroIconPath = (name, size) => {
-	name = name.replace(/npc_dota_hero_/gi, '') + '_';
-	return 'http://cdn.dota2.com/apps/dota2/images/heroes/' + name + size;
-}
+// // Economy Interface Methods
+// dotaWebAPI.prototype.getItems = (lang) => {
+// 	let parameters = {
+// 		"key" : this.apiKey,
+// 		"language" : lang
+// 	}
+// 	return fetch(API_URL + ECONOMY_INTERFACE + APP_ID + "/GetGameItems/v1" + query(parameters))
+// 			.then(response => JSON.parse(response))
+// }
 
-module.exports = dotaWebAPI;
+// dotaWebAPI.prototype.getHeroes = (lang, ifItemized) => {
+// 	let parameters = {
+// 		"key" : this.apiKey,
+// 		"language" : lang,
+// 		"itemizedonly" : ifItemized
+// 	}
+// 	return fetch(API_URL + ECONOMY_INTERFACE + APP_ID + "/GetHeroes/v1" + query(parameters))
+// 			.then(response => JSON.parse(response))
+// }
+
+// dotaWebAPI.prototype.getItemIconPath = (name) => {
+// 	name = name.replace(/item_/gi, '') + '_';
+// 	return 'http://cdn.dota2.com/apps/dota2/images/items/' + name + 'lg.png';
+// }
+// // sb.png, lg.png, full.png, vert.jpg
+// dotaWebAPI.prototype.getHeroIconPath = (name, size) => {
+// 	name = name.replace(/npc_dota_hero_/gi, '') + '_';
+// 	return 'http://cdn.dota2.com/apps/dota2/images/heroes/' + name + size;
+// }
+
+module.exports = {
+	dotaWebAPI: dotaWebAPI
+};
 
 // utility
 let query = (parameters) => {
--- src/test.js
+++ src/test.js
@@ -1,18 +1,12 @@
-/* Module */
-var chai = require('chai');
-var dotaWebAPI = require('../index'); // module entry point
+import { assert, expect } from 'chai';
 
-/* Chai API */
-var assert = chai.assert;
-var expect = chai.expect;
-var should = chai.should();
+import { dotaWebAPI } from './api';
 
-var myAPIKey = "<your own Steam API Key>";
-var api = new dotaWebAPI(myAPIKey);
+let api = new dotaWebAPI("1F72FF9BF1B0C1DF25A8CFA722DA0019");
 
 // fetches a existing match from the api
 describe("getMatchDetails()", () => {
-	var matchId = 3574415631
+	let matchId = 3574415631
 
 	it("should return a valid response containing the correct properties of the JSON object", done => {
 		api.getMatchDetails(matchId)
